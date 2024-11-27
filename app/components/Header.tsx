3af128beb19be25596738c2d0f6de8d0249fb1f4
Hari Prasad Sanjel <121128703+hpsanjel@users.noreply.github.com>
2024-11-27 22:50:24 +0100

    styled mobile menu

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -25,7 +25,7 @@ export default function Header({ isMenuOpen, toggleMenu }: HeaderProps) {
 		<motion.header className={`fixed w-full z-50 transition-colors duration-300 ${isScrolled ? "bg-white shadow-md" : "bg-transparent"}`} initial={{ y: -100 }} animate={{ y: 0 }} transition={{ duration: 0.5 }}>
 			<div className="container mx-auto px-4 py-4 flex justify-between items-center">
 				<div className="flex items-center space-x-4 cursor-pointer group">
-					<Image src="/kns_logo_rect.png" alt="KNS Entertainment" width={400} height={200} className="w-auto h-16 bg-slate-200 group-hover:bg-slate-100" />
+					<Image src="/kns_logo_rect.png" alt="KNS Entertainment" width={400} height={200} className="w-auto h-16 rounded-md bg-slate-200 group-hover:bg-slate-100" />
 					<span className={`hidden sm:block text-2xl font-bold text-primary ${isScrolled ? "text-black " : "text-slate-200"}`}>KNS Entertainment</span>
 				</div>
 				<nav className="hidden md:flex space-x-6">
@@ -72,7 +72,7 @@ export default function Header({ isMenuOpen, toggleMenu }: HeaderProps) {
 
 function NavLink({ href, children, onClick }: { href: string; children: React.ReactNode; onClick?: () => void }) {
 	return (
-		<a href={href} className="text-gray-600 hover:text-primary transition-colors duration-300 py-2" onClick={onClick}>
+		<a href={href} className="text-gray-600 hover:bg-slate-100 w-full text-center hover:text-red-600 transition-colors duration-300 py-2" onClick={onClick}>
 			{children}
 		</a>
 	);
