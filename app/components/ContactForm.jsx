d3eef3ce87ea16124096eb80a8506320c5329a43
Hari Prasad Sanjel <121128703+hpsanjel@users.noreply.github.com>
2024-12-01 10:24:58 +0100

    Email is working now, also form sent feedback is updated

--- app/components/ContactForm.jsx
+++ app/components/ContactForm.jsx
@@ -3,7 +3,8 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import { Send, Loader2 } from "lucide-react";
+import { Send, Loader2, Smile } from "lucide-react";
+import { set } from "mongoose";
 
 const ContactForm = () => {
 	const [formData, setFormData] = React.useState({
@@ -13,6 +14,7 @@ const ContactForm = () => {
 	});
 	const [isSubmitting, setIsSubmitting] = React.useState(false);
 	const [errors, setErrors] = React.useState({});
+	const [message, setMessage] = React.useState("");
 
 	const validateForm = () => {
 		const newErrors = {};
@@ -48,15 +50,28 @@ const ContactForm = () => {
 		if (Object.keys(newErrors).length === 0) {
 			setIsSubmitting(true);
 			try {
-				// Simulated API call
-				await new Promise((resolve) => setTimeout(resolve, 1500));
-				console.log("Form submitted:", formData);
-				// Reset form
-				setFormData({
-					email: "",
-					subject: "",
-					message: "",
+				const response = await fetch("https://api.web3forms.com/submit", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						access_key: "cf9615fd-9901-4e6b-a50e-241719bbcda6",
+						...formData,
+					}),
 				});
+
+				if (response.ok) {
+					setMessage("Thanks for reaching out! We'll get back to you soon.");
+					// Reset form
+					setFormData({
+						email: "",
+						subject: "",
+						message: "",
+					});
+				} else {
+					console.error("Error submitting form:", await response.json());
+				}
 			} catch (error) {
 				console.error("Error submitting form:", error);
 			} finally {
@@ -71,70 +86,78 @@ const ContactForm = () => {
 		<Card className="bg-white shadow-lg mx-4">
 			<CardContent className="p-6">
 				<h3 className="text-xl font-semibold text-gray-800 mb-2">Have Something to Say?</h3>
-
-				<form onSubmit={handleSubmit} className="space-y-6">
-					{/* Email Input */}
-					<div className="">
-						<div className="relative">
-							<Input
-								type="email"
-								name="email"
-								placeholder="Your Email"
-								value={formData.email}
-								onChange={handleChange}
-								className={`w-full p-3 rounded-lg bg-gray-50 border transition-colors focus:bg-white
+				<p className="text-gray-600 mb-6">We'd love to hear from you. Fill out the form below and we'll get back to you as soon as possible.</p>
+				{message ? (
+					<div className="flex flex-col items-center justify-center p-6 space-y-4 border border-green-700 bg-green-50">
+						<Smile className="h-12 w-12 text-green-700" />
+						<p className="text-green-700 text-center">{message}</p>
+						<Button onClick={() => setMessage("")}>Send another message</Button>
+					</div>
+				) : (
+					<form onSubmit={handleSubmit} className="space-y-6">
+						{/* Email Input */}
+						<div className="">
+							<div className="relative">
+								<Input
+									type="email"
+									name="email"
+									placeholder="Your Email"
+									value={formData.email}
+									onChange={handleChange}
+									className={`w-full p-3 rounded-lg bg-gray-50 border transition-colors focus:bg-white
                   ${errors.email ? "border-red-500 focus:border-red-500" : "border-gray-200 focus:border-blue-500"}`}
-							/>
-							{errors.email && <p className="absolute -bottom-5 left-0 text-xs text-red-500">{errors.email}</p>}
+								/>
+								{errors.email && <p className="absolute -bottom-5 left-0 text-xs text-red-500">{errors.email}</p>}
+							</div>
 						</div>
-					</div>
 
-					{/* Subject Input */}
-					<div className="">
-						<div className="relative">
-							<Input
-								type="text"
-								name="subject"
-								placeholder="Subject"
-								value={formData.subject}
-								onChange={handleChange}
-								className={`w-full p-3 rounded-lg bg-gray-50 border transition-colors focus:bg-white
+						{/* Subject Input */}
+						<div className="">
+							<div className="relative">
+								<Input
+									type="text"
+									name="subject"
+									placeholder="Subject"
+									value={formData.subject}
+									onChange={handleChange}
+									className={`w-full p-3 rounded-lg bg-gray-50 border transition-colors focus:bg-white
                   ${errors.subject ? "border-red-500 focus:border-red-500" : "border-gray-200 focus:border-blue-500"}`}
-							/>
-							{errors.subject && <p className="absolute -bottom-5 left-0 text-xs text-red-500">{errors.subject}</p>}
+								/>
+								{errors.subject && <p className="absolute -bottom-5 left-0 text-xs text-red-500">{errors.subject}</p>}
+							</div>
 						</div>
-					</div>
 
-					{/* Message Textarea */}
-					<div className="">
-						<div className="relative">
-							<Textarea
-								name="message"
-								placeholder="Your Message"
-								value={formData.message}
-								onChange={handleChange}
-								className={`w-full p-3 h-32 rounded-lg bg-gray-50 border transition-colors focus:bg-white resize-none
+						{/* Message Textarea */}
+						<div className="">
+							<div className="relative">
+								<Textarea
+									name="message"
+									placeholder="Your Message"
+									value={formData.message}
+									onChange={handleChange}
+									className={`w-full p-3 h-32 rounded-lg bg-gray-50 border transition-colors focus:bg-white resize-none
                   ${errors.message ? "border-red-500 focus:border-red-500" : "border-gray-200 focus:border-blue-500"}`}
-							/>
-							{errors.message && <p className="absolute -bottom-5 left-0 text-xs text-red-500">{errors.message}</p>}
+								/>
+								{errors.message && <p className="absolute -bottom-5 left-0 text-xs text-red-500">{errors.message}</p>}
+							</div>
 						</div>
-					</div>
 
-					{/* Submit Button */}
-					<Button type="submit" disabled={isSubmitting} className="w-full bg-slate-700 hover:bg-black text-white py-3 rounded-lg transition-all duration-200 flex items-center justify-center space-x-2">
-						{isSubmitting ? (
-							<>
-								<Loader2 className="h-5 w-5 animate-spin" />
-								<span>Sending...</span>
-							</>
-						) : (
-							<>
-								<Send className="h-5 w-5" />
-								<span>Send Message</span>
-							</>
-						)}
-					</Button>
-				</form>
+						{/* Submit Button */}
+						<Button type="submit" disabled={isSubmitting} className="w-full bg-slate-700 hover:bg-black text-white py-3 rounded-lg transition-all duration-200 flex items-center justify-center space-x-2">
+							{isSubmitting ? (
+								<>
+									<Loader2 className="h-5 w-5 animate-spin" />
+									<span>Sending...</span>
+								</>
+							) : (
+								<>
+									<Send className="h-5 w-5" />
+									<span>Send Message</span>
+								</>
+							)}
+						</Button>
+					</form>
+				)}
 			</CardContent>
 		</Card>
 	);
