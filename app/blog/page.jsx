0c97855aa20e51c27f8b457f038f727bf6fb6443
Hari Prasad Sanjel <121128703+hpsanjel@users.noreply.github.com>
2024-12-03 18:23:43 +0100

    removed blog page

--- app/blog/page.jsx
+++
@@ -1,8 +0,0 @@
-import { getBlogData } from "@/lib/getBlogData";
-import Blog from "@/components/Blog";
-
-export default async function Page() {
-	const { featuredArticle, recentArticles, popularArticles } = await getBlogData();
-
-	return <Blog featuredArticle={featuredArticle} recentArticles={recentArticles} popularArticles={popularArticles} />;
-}
